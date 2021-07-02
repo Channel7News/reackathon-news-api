fce16591109c5e1fe47a3719329f6b75d07ac2bb
whywesmurfing <raymond.doan94@gmail.com>
2021-07-02 11:35:01 +1000

    adjustments made to retrieved data

--- src/server.js
+++ src/server.js
@@ -17,7 +17,6 @@ router.get("/everything/:query", (req, res) => {
     `https://newsapi.org/v2/everything?q=${searchValue}&apiKey=4528ed13b69744e581384c6fa39e00de`
   )
     .then((response) => response.json())
-    .then((data) => res.send(data));
 });
 
 router.get("/headlines/:category/:countryCode", (req, res) => {
@@ -27,7 +26,6 @@ router.get("/headlines/:category/:countryCode", (req, res) => {
     `https://newsapi.org/v2/top-headlines?category=${category}&country=${countryCode}&apiKey=4528ed13b69744e581384c6fa39e00de`
   )
     .then((response) => response.json())
-    .then((data) => res.send(data));
 });
 
 app.use(bodyParser.json());
